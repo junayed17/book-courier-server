d9fead883bdc1e88b0e60f722150f44db1bd0add
junayed17 <junayedahmednipun@gmail.com>
2025-12-18 15:47:52 +0600

    payment api added

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT;
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS_WORD}@cluster0.vdcsgkx.mongodb.net/?appName=Cluster0`;
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const FRONTEND_DOMAIN = process.env.FRONTEND_DOMAIN;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -18,6 +21,7 @@ const client = new MongoClient(uri, {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 async function run() {
   try {
@@ -65,15 +69,11 @@ async function run() {
       }
     });
 
-
-
-    // latest book and all book api basend on query 
+    // latest book and all book api basend on query
     app.get("/books", async (req, res) => {
-      let { limit,sort} = req.query;
+      let { limit, sort } = req.query;
 
-
-
-      limit = limit ? parseInt(limit) : 0; 
+      limit = limit ? parseInt(limit) : 0;
       const sortProperty = sort ? { price: parseInt(sort) } : { createdAt: -1 };
       const query = Books.find().sort(sortProperty).project({
         title: 1,
@@ -88,154 +88,172 @@ async function run() {
         query.limit(limit);
       }
 
-
       const result = await query.toArray();
       res.status(200).send(result);
     });
 
+    // post details api
+    app.get("/books/:bookId", async (req, res) => {
+      const { bookId } = req.params;
+      const result = await Books.findOne({ _id: new ObjectId(bookId) });
+      res.send(result);
+    });
 
-    // post details api 
-    app.get("/books/:bookId",async(req,res)=>{
-      const {bookId}=req.params;
-      const result =await Books.findOne({ _id:new ObjectId(bookId) });
-      res.send(result)
-    })
-
-
-    // wishList api 
-    app.post("/wishListBook",async(req,res)=>{
-      const wishBook=req.body;
+    // wishList api
+    app.post("/wishListBook", async (req, res) => {
+      const wishBook = req.body;
       const result = await wishList.insertOne(wishBook);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-
-    // orders api 
-    app.post("/orders",async(req,res)=>{
-      const orderData=req.body;
+    // orders api
+    app.post("/orders", async (req, res) => {
+      const orderData = req.body;
       console.log(orderData);
-      
-      const result=await orders.insertOne(orderData)
-      res.send(result)
-    })
 
-
+      const result = await orders.insertOne(orderData);
+      res.send(result);
+    });
 
     // my order api and order on my books api
-    app.get("/myOrders",async(req,res)=>{
+    app.get("/myOrders", async (req, res) => {
       const userEamil = req.query;
       console.log(userEamil);
       const findQuery = {};
 
       if (userEamil.lEmail) {
-        findQuery.sellerEmail=userEamil.lEmail;
-      }
-      else{
+        findQuery.sellerEmail = userEamil.lEmail;
+      } else {
         findQuery.email = userEamil.uEmail;
       }
-      
+
       const result = await orders
         .find(findQuery)
-        .sort({ orderAt :-1})
+        .sort({ orderAt: -1 })
         .toArray();
-       res.send(result)
-    })
+      res.send(result);
+    });
 
-
-    // all post api 
-    app.get("/allPost",async(req,res)=>{
-      const result = await Books.find().sort({ createdAt: -1 }).toArray();
+    // api for payment 
+    app.get("/payment/:id",async(req,res)=>{
+      const {id}=req.params;
+      const result = await orders.findOne({ bookId: id });  
       res.send(result)
     })
 
+    // all post api
+    app.get("/allPost", async (req, res) => {
+      const result = await Books.find().sort({ createdAt: -1 }).toArray();
+      res.send(result);
+    });
 
-    // book post approve api 
-      app.patch("/post/:id", async (req, res) => {
-        const bookId = req.params.id;
-        const {isApprove}=req.body
-        console.log(bookId, isApprove);
-        const updatedQuery = {
-          $set: {
-            isApprove,
-          },
-        };
-        const findQuery = {
-          _id:new ObjectId(bookId)
-        };
+    // book post approve api
+    app.patch("/post/:id", async (req, res) => {
+      const bookId = req.params.id;
+      const { isApprove } = req.body;
+      console.log(bookId, isApprove);
+      const updatedQuery = {
+        $set: {
+          isApprove,
+        },
+      };
+      const findQuery = {
+        _id: new ObjectId(bookId),
+      };
 
-        const result = await Books.updateOne(findQuery,updatedQuery)
-        res.send(result);
-      });
+      const result = await Books.updateOne(findQuery, updatedQuery);
+      res.send(result);
+    });
 
+    // delete book by admin
+    app.delete("/book/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await Books.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
 
-
-      // delete book by admin 
-      app.delete("/book/:id",async(req,res)=>{
-        const {id}=req.params;      
-        const result=await Books.deleteOne({_id:new ObjectId(id)})
-        res.send(result)
-      })
-
-
-    // delivery status updated api 
-    app.patch("/order/updateStatus",async(req,res)=>{
-      const {id}=req.query;
+    // delivery status updated api
+    app.patch("/order/updateStatus", async (req, res) => {
+      const { id } = req.query;
       const { updatedStatus } = req.body;
-      console.log(id,updatedStatus);
+      console.log(id, updatedStatus);
       const result = await orders.updateOne(
         { bookId: id },
         { $set: { status: updatedStatus } }
       );
-      console.log(id, updatedStatus,result);
-      res.send(result)
+      console.log(id, updatedStatus, result);
+      res.send(result);
     });
 
-
-
-
-    // find all user api 
-    app.get("/users",async(req,res)=>{
+    // find all user api
+    app.get("/users", async (req, res) => {
       const result = await Users.find().toArray();
-      res.send(result)
-    })
-
+      res.send(result);
+    });
 
     // user role update api
-    app.patch("/users/:id",async(req,res)=>{
-      const {id}=req.params;
-      const {role}=req.body;
-      const findQuery={
-        _id:new ObjectId(id)
-      }
-      const updateQuery={
-       $set: {role:role}
-      }
-      const result=await Users.updateOne(findQuery,updateQuery)
-      res.send(result)
-    })
+    app.patch("/users/:id", async (req, res) => {
+      const { id } = req.params;
+      const { role } = req.body;
+      const findQuery = {
+        _id: new ObjectId(id),
+      };
+      const updateQuery = {
+        $set: { role: role },
+      };
+      const result = await Users.updateOne(findQuery, updateQuery);
+      res.send(result);
+    });
 
-    // findSpecifiqUser api 
-    app.get("/user",async(req,res)=>{
-      const {email}=req.query;
+    // findSpecifiqUser api
+    app.get("/user", async (req, res) => {
+      const { email } = req.query;
       const result = await Users.findOne(
         { email },
         { projection: { role: 1, _id: 0 } }
-      )
+      );
       console.log(email);
-      
-      res.send(result)
-    })
 
+      res.send(result);
+    });
 
-    // search book api 
-    app.get("/book",async(req,res)=>{
-      const {bookName}=req.query;
+    // search book api
+    app.get("/book", async (req, res) => {
+      const { bookName } = req.query;
       const result = await Books.find({
         title: { $regex: bookName, $options: "i" },
-      }).toArray()
-      res.send(result)
-    })
+      }).toArray();
+      res.send(result);
+    });
 
+    // payment Api
+    app.post("/payment-checkout-session", async (req, res) => {
+      const paymentData = req.body;
+      
+      const amount = parseInt(paymentData.cost)*100;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "usd",
+              unit_amount: amount,
+              product_data: {
+                name: `pay for: ${paymentData.bookName}`,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        metadata: {
+          parcelId: paymentData.bookId,
+        },
+        customer_email: paymentData.senderEmail,
+        success_url: `${process.env.FRONTEND_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.FRONTEND_DOMAIN}/dashboard/payment-cancelled`,
+      });
+      res.send({ url: session.url });
+    });
 
     app.get("/", (req, res) => {
       res.send("database is running");
